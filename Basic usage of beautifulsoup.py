7bf0578d096fb6420974ddce7a6589e8025cb345
baixinchen <34299647+baixinchen0811@users.noreply.github.com>
2018-10-02 21:58:36 +0800

    #

--- Basic usage of beautifulsoup.py
+++ Basic usage of beautifulsoup.py
@@ -3,8 +3,12 @@ from urllib.request import urlopen
 
 # if has Chinese, apply decode()
 html = urlopen("https://morvanzhou.github.io/static/scraping/basic-structure.html").read().decode('utf-8')
-# print(html)
+print(html)
 
-soup = BeautifulSoup(html, features='lxml')
+soup = BeautifulSoup(html, features='html.parser')
 print(soup.h1)
 print('\n', soup.p)
+#
+all_href = soup.find_all('a')
+all_href = [l['href'] for l in all_href]
+print('\n', all_href)
